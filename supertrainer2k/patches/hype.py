de7b003b0959f27bf2b0f2eb1961f792fbb48132
Jade <jpritsk@purdue.edu>
2024-01-10 15:06:02 +0000

    hype

--- supertrainer2k/patches/hype.py
+++ supertrainer2k/patches/hype.py
@@ -7,10 +7,11 @@ from .quantemb import QuantEmbedding
 # https://arxiv.org/abs/2212.08853
 
 class HyPeLayer(nn.Module):
-    def __init__(self, orig: nn.Module, sigma: float):
+    def __init__(self, orig: nn.Module, sigma: float, normal: bool):
         super().__init__()
         self.orig = orig
         self.sigma = sigma
+        self.noise = torch.randn_like if normal else torch.rand_like
     
     def forward(self, hidden_states,**kwargs):
         eps = torch.rand_like(hidden_states)
@@ -20,8 +21,9 @@ class HyPeLayer(nn.Module):
     def unpatch(self):
         return self.orig
         
-def apply_hype(model, sigma, layer_class):
-    patch_model(model, [layer_class], lambda m: HyPeLayer(m, sigma))
+def apply_hype(model, layer_class, sigma=1e-5, normal=False):
+    assert layer_class is not None
+    patch_model(model, [layer_class], lambda m: HyPeLayer(m, sigma, normal))
 
 def remove_hype(model):
     patch_model(model, [HyPeLayer], lambda m: m.unpatch())
